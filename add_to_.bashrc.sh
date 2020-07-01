284cacd4189c646369db24d02b291aad2db4cd69
Michal K <32823477+michalk0128@users.noreply.github.com>
2020-07-01 10:03:30 +0200

    Modified add_to_.bashrc.sh

--- add_to_.bashrc.sh
+++ add_to_.bashrc.sh
@@ -1,3 +1,5 @@
+# Add the following to .bashrc
+
 function bak() {
     for file_source in $(ls -d $@ 2>/dev/null | grep -vE ".*.bak_202[0-9]-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])_[0-9]+(.tar.gz)?$")
     do
