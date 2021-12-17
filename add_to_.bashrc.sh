924275d3c38470288238e959e297e7aa212ca856
Michal K <32823477+michalk0128@users.noreply.github.com>
2021-12-17 22:46:27 +0100

    Update add_to_.bashrc.sh

--- add_to_.bashrc.sh
+++ add_to_.bashrc.sh
@@ -1,15 +1,17 @@
-# Add the following to .bashrc
+# Add the following to ~/.bashrc
+# Usage:
+# bak filename; bak dirname
 
 function bak() {
-for file_source_input in $@; do
-        file_source_all=$(readlink -f ${file_source_input})
-        file_source_dirname=$(dirname $file_source_all)
-        for file_source_1_find in $(find ${file_source_dirname} -mindepth 1 -maxdepth 1 -name "$(basename ${file_source_all})" | sed 's/^\/$//g;s/^\/\.$//g' | grep -vE ".*.bak_202[0-9]-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])_[0-9]+(.tar.gz)?$"); do
+for FILE_SOURCE_INPUT in $@; do
+        FILE_SOURCE_ALL=$(readlink -f ${FILE_SOURCE_INPUT})
+        FILE_SOURCE_DIRNAME=$(dirname $FILE_SOURCE_ALL)
+        for FILE_SOURCE_1_FIND in $(find ${FILE_SOURCE_DIRNAME} -mindepth 1 -maxdepth 1 -name "$(basename ${FILE_SOURCE_ALL})" | sed 's/^\/$//g;s/^\/\.$//g' | grep -vE ".*.bak_202[0-9]-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])_[0-9]+(.tar.gz)?$"); do
                 BAK_CREATED=0
                 for i in $(seq -w 1 99); do
-                        FILE_PATH=$(dirname ${file_source_1_find})
-                        FILE_SOURCE_NO_PATH=$(basename ${file_source_1_find})
-                        FILE_BAK_NO_PATH="${FILE_SOURCE_NO_PATH}.bak_$(date "+%F")_${i}"
+                        FILE_PATH=$(dirname ${FILE_SOURCE_1_FIND})
+                        FILE_SOURCE_NO_PATH=$(basename ${FILE_SOURCE_1_FIND})
+                        FILE_BAK_NO_PATH="${FILE_SOURCE_NO_PATH}.bak_$(date "+%F_%H-%M-%S")"
                         FILE_SOURCE_WITH_PATH="${FILE_PATH}/${FILE_SOURCE_NO_PATH}"
                         FILE_BAK_WITH_PATH="${FILE_PATH}/${FILE_BAK_NO_PATH}"
                         if [ ! -e ${FILE_BAK_WITH_PATH} ] && [ ${BAK_CREATED} -ne 1 ]; then
